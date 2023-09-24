805c6d5c64a9a41f63a5e8cab034f24a58291b77
Jorge Suin Yum Gonzales <117481269+jsyumg@users.noreply.github.com>
2023-09-24 18:43:11 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,7 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    member.innerHTML = "Member";
+    member.style.color = "black";
+    member.style.fontWeight = "bold";
+    member.style.textDecoration = "underline";
+}
